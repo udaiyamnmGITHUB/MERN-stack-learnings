66fc6f46a0eddd8ed2c344da4b4506443c68260c
udhandapani <udhandapani@deloitte.co.uk>
2023-11-01 00:47:19 +0000

    jwt logic is implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
-
+const User = require("../../models/User.model");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { checkSchema, validationResult } = require("express-validator");
 
 const router = express.Router();
@@ -28,37 +32,70 @@ router.post(
       email: {
         errorMessage: "Invalid email",
         notEmpty: true,
-        isEmail: true
+        isEmail: true,
       },
       password: {
         errorMessage: "Invalid password",
         notEmpty: true,
         isLength: {
           options: { min: 8, max: 15 },
-        }
-      }
+        },
+      },
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     // validates the user details from the client
     if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
-    
-    // validates the user details with the mongoDB
 
-    // see if the username/ email already exists
-    
-    // get user avatar
+    try {
+      // validates the user details with the mongoDB
+      const { firstName, lastName, email, password } = req.body;
 
-    //Encrypt the password and insert in DB
+      let userFromDB = await User.findOne({ email: email });
+      // see if the username/ email already exists
+      if (userFromDB) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "email is already exist" }] });
+      }
+      // get user avatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "png",
+        d: "mm",
+      });
+      //Encrypt the password and insert in DB
 
-    // return JSONWeb token
+      let newUserToInsert = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+        avatar,
+      });
+      const salt = await bcrypt.genSalt(10);
+      newUserToInsert.password = await bcrypt.hash(password, salt);
+      await newUserToInsert.save();
+      // return JSONWeb token
+      const payload = {
+        user: {
+          id: newUserToInsert.id,
+        },
+      };
+      jwt.sign(payload, config.get("jwtSecret"), {expiresIn:360000},
+      (err, token) =>{
+        if(err) throw err;
+        res.json({token});
+      });
 
-
-    const newUser = (({ firstName, lastName, email }) => ({ firstName, lastName, email}))(req.body);
-    res.send({"msg":"User created successfully", data: newUser});
+     // res.send({ msg: "User created successfully", data: newUserToInsert });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send("something went wrong");
+    }
   }
 );
 
